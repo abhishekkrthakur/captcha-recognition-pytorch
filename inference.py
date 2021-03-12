2bf3cdeaafb3d3dc9413e3930214d58ad2bd6bc4
Nanohana <56324869+GabrielDornelles@users.noreply.github.com>
2021-03-12 19:39:28 -0300

    integration with config and model file

--- inference.py
+++ inference.py
@@ -7,52 +7,12 @@ import os
 from sklearn import preprocessing
 import time
 import albumentations
+import config
+from model import CaptchaModel
 
-class CaptchaModel(nn.Module):
-
-    def __init__(self, num_chars):
-        super(CaptchaModel, self).__init__()
-
-        self.conv_1 = nn.Conv2d(3, 256, kernel_size=(3, 6), padding=(1, 1))
-        self.pool_1 = nn.MaxPool2d(kernel_size=(2, 2))
-        self.conv_2 = nn.Conv2d(256, 64, kernel_size=(3, 6), padding=(1, 1))
-        self.pool_2 = nn.MaxPool2d(kernel_size=(2, 2))
-        self.linear_1 = nn.Linear(1152, 64)
-        self.drop_1 = nn.Dropout(0.2)
-        self.lstm = nn.GRU(64, 32, bidirectional=True, num_layers=2, dropout=0.25, batch_first=True)
-        self.output = nn.Linear(64, num_chars + 1)
-
-    def forward(self, images, targets=None):
-        bs, _, _, _ = images.size()
-        x = F.relu(self.conv_1(images))
-        x = self.pool_1(x)
-        x = F.relu(self.conv_2(x))
-        x = self.pool_2(x)
-        x = x.permute(0, 3, 1, 2)
-        x = x.view(bs, x.size(1), -1)
-        x = F.relu(self.linear_1(x))
-        x = self.drop_1(x)
-        x, _ = self.lstm(x)
-        x = self.output(x)
-        x = x.permute(1, 0, 2)
-
-        if targets is not None:
-            log_probs = F.log_softmax(x, 2)
-            input_lengths = torch.full(
-                size=(bs,), fill_value=log_probs.size(0), dtype=torch.int32
-            )
-            target_lengths = torch.full(
-                size=(bs,), fill_value=targets.size(1), dtype=torch.int32
-            )
-            loss = nn.CTCLoss(blank=0)(
-                log_probs, targets, input_lengths, target_lengths
-            )
-            return x, loss
-
-        return x, None
 
 model = CaptchaModel(33)
-device = torch.device("cuda")
+device = torch.device(config.DEVICE)
 model.to(device)
 
 PATH = "./your_model_state_dict.pth" 
@@ -83,7 +43,7 @@ def return_model_answer(img_path):
     img = img_path
     image = Image.open(img_path).convert("RGB")
     image = image.resize(
-                (70, 25), resample=Image.BILINEAR
+                (config.IMAGE_WIDTH, config.IMAGE_HEIGHT), resample=Image.BILINEAR 
             )
     image = np.array(image)
 
